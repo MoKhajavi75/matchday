7f274b4ce73e65e909dc51ddddd7f0c7d67c06dd
MoKhajavi75 <khajavi.contact@gmail.com>
2026-02-06 03:09:22 +0330

    chore: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { readFileSync } from 'fs';
+import { defineConfig } from 'vite';
+
+const pkg = JSON.parse(readFileSync('./package.json', 'utf-8'));
+
+export default defineConfig({
+  define: {
+    __APP_VERSION__: JSON.stringify(pkg.version)
+  },
+  server: {
+    port: 3000,
+    open: false
+  }
+});
