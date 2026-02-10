517ffc7c11006709181b97af64bbd4be9030caf8
MoKhajavi75 <khajavi.contact@gmail.com>
2026-02-10 17:46:53 +0330

    feat: add single file build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { readFileSync } from 'fs';
 import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 const pkg = JSON.parse(readFileSync('./package.json', 'utf-8'));
 
 export default defineConfig({
+  plugins: [viteSingleFile()],
   define: {
     __APP_VERSION__: JSON.stringify(pkg.version)
   },
